2ebfd7814fd9cd3a1415b7d75391f28136d24361
Tohid Khan <tohidkhan9050482152@gmail.com>
2025-12-30 23:46:39 +0530

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,13 +1,7 @@
-import React from 'react';
-import TohidGame from './TohidGame'; // Ye tumhare game file ko link karega
+import TohidGame from './TohidGame';
 
 function App() {
-  return (
-    <div className="App">
-      {/* Yahan hum React ko bol rahe hain ki Game dikhao */}
-      <TohidGame />
-    </div>
-  );
+  return <TohidGame />;
 }
 
 export default App;
